faf5c319f4522ad839694127ea7dd3a122dc1682
Atindra Mishra <81668822+Atindra11Mishra@users.noreply.github.com>
2022-12-15 00:23:49 +0530

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/Atindra11Mishra",
+    "name": "Atindra Mishra",
+    "role": "Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and also learning about new technologies, I like to follow my hobbies in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Atindra11Mishra",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently pursuing my Bachelor of Technology in Computer Science from Shree Vaishnav Vidyapeeth Vishwavidyalaya. I am a self taught Web Developer, currently diving deeper into Full Stack Developement. I believe that to be successful in life, one needs to be obsessive with their dreams and should never give up.",
+    "address":"India",
+    "website":"https://github.com/Atindra11Mishra",
+    "education":[
+      {
+        "UniversityName":"Shree Vaishnav Vidyapeeth Vishwavidyalaya",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2025",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2025",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"A guy who loves programming"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"A guy who loves programming"
+      }
+    ]
+  }
+  
+  export default resumeData
